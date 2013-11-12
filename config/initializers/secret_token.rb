ae10e26f93d556b0d8e5156fb185ac4aa632051c
Granger + Anthony <Granger+Anthony@appacademy.io>
2013-11-12 09:21:22 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstRoutes::Application.config.secret_token = '5fa9f73718a93fe898b5936fd434d98b011b59e87bb75548e2605c3d36b7f9c32099f1f695835ab898cd17f172908ea67d7866d44f19ef675d9b794d29fdcdbc'
